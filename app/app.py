2db07a7880e9bf4f17fb42d95435ebf7fd7ddbf4
kslim1025 <kslim1025@naver.com>
2022-04-14 17:03:39 +0900

    git push origin main

--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
 import sys
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# config.py 설정파일
+app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.secret_key = 'flasknotewithsqlalchemy'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
--- app/config.py
+++ app/config.py
@@ -1,5 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, Column, Integer, String, DateTime, Float, MetaData
+import pymssql
 
-#MSSQL 연동
-#engine = create_engine('mssql+pymsql://username:passwd@host/database',echo=True)
-engine = create_engine('mssql+pymsql://ajis:@jis2021!@211.117.60.37/magesty',echo=True)
+# pip install pymysql
+# Database parameter
+host = "211.117.60.37"         
+user = "ajis"          
+password = "@jis2021!"    
+database = "ajis"     
+
+# Database connection
+db=pymssql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        autocommit=True)
+
+# pymssql
+#engine = create_engine('mssql+pymssql://{user}:{password}@{host}/{database}')
+cursor=db.cursor()
+print(cursor)
