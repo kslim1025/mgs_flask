1515170c8278974528f95274c6b0e46ffe22018b
kslim1025 <kslim1025@naver.com>
2022-04-14 13:53:39 +0900

    git magesty commit

--- app/main/app.py
+++ app/main/app.py
@@ -6,8 +6,22 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'sibal'
+    return '''
+    <DOCTYPE html>
+    <head>
+    <title>Magesty</title>
+    </head>
+    <body>
+    <p>간단한 테스트</p>
+    </body>
+    </html>
+    '''
+
+@app.route('/user')
+def user():
+    return 'hello'
 
 if __name__=='__main__':
     app.run(debug=True)
