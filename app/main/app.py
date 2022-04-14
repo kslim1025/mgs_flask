61a1fe8500f1b1eadf1c437e877c2ecb959d64ec
kslim1025 <kslim1025@naver.com>
2022-04-14 14:09:17 +0900

    git mgs_flask commit

--- app/main/app.py
+++ app/main/app.py
@@ -2,26 +2,15 @@
 # 필요한 패키지 목록
 
 from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return '''
-    <DOCTYPE html>
-    <head>
-    <title>Magesty</title>
-    </head>
-    <body>
-    <p>간단한 테스트</p>
-    </body>
-    </html>
-    '''
+def index():
+    return render_template("login.html")
 
-@app.route('/user')
-def user():
-    return 'hello'
+
 
 if __name__=='__main__':
     app.run(debug=True)
