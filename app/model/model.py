8bec107e68259fb1cbe3294597e2db185008473a
kslim1025 <kslim1025@naver.com>
2022-04-15 16:20:20 +0900

    git mgs commit

--- app/model/model.py
+++ app/model/model.py
@@ -4,8 +4,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'a_MagestyMember'
-    
-    sort = db.Column(db.Integer, primary_key=True)
+    sort = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id = db.Column(db.String(50))
     email = db.Column(db.String(50))
     pwd = db.Column(db.String(128))  
+
+class UserRegist(db.Model):
+    __tablename__= 'a_MagestyMember'
+    sort = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = = db.Column(db.String(50))
+    group_cd = db.Column(db.String(50))
+    sex_fg = db.Column(db.String(2))
+    name = db.Column(db.String(50))
+    add1 = db.Column(db.String(150))
+    tel_no2 = db.Column(db.String(150))
+    email = db.Column(db.String(50))
+    pwd = db.Column(db.String(128)) 
+   
